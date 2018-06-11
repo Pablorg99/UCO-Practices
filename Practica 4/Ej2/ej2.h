e93301f39627443b252f27c6524294a225d6f3fe
Pablorg99 <i72rogup@uco.es>
2018-06-11 11:00:58 +0200

    P4 Ej2 Comentado

--- Practica 4/Ej2/ej2.h
+++ Practica 4/Ej2/ej2.h
@@ -23,33 +23,47 @@ Al terminar el programa, deberá liberar la memoria usada.
 ------------------------------------------------------------------------------*/
 
 
-  /**----------------------------------------------------------------------------
-
+  /*----------------------------------------------------------------------------
+  Reserva memoria para un vector de estructuras de nEle elementos (argumento) y
+  devuelve un puntero al inicio del mismo.
   ----------------------------------------------------------------------------*/
   ficha * reservarMemoriaVector(int nEle);
 
   /*----------------------------------------------------------------------------
-
+  Abre el archivo cuyo nombre se ha pasado como argumento. Con fseek, coloco el
+  cursor del fichero al final del mismo, y como ftell() indica el número de
+  bytes desde el inicio hasta la posición del cursor, dividiendo dicho número
+  por sizeof(ficha), obtengo el número de structs que tiene el fichero. Este
+  numero se pasa como argumento a reservarMemoriaVector().
+  Después de colocar el cursor al principio del fichero de nuevo, paso el fichero
+  binario al vector dinámico reservado previamente a través de fread(). La función
+  devuelve un puntero al inicio del vector.
   ----------------------------------------------------------------------------*/
   ficha * getArrayFromFile (char *file, int *nEle);
 
   /*----------------------------------------------------------------------------
-
+  El algoritmo de ordenación burbuja implementado de manera que mediante un
+  puntero a funcion, puedo llamar a la función desde el main con esMayor o
+  esMenor, para ordenar el vector de manera ascendente o descendente.
+  El vector se ordena en funcion del campo DNI del struct, el primer elemento
+  del vector (si se ordena de manera ascendente) será aquel cuyo número de DNI
+  sea el menor
   ----------------------------------------------------------------------------*/
   void burbuja(ficha* vector, int izda, int dcha, int(*comparacion)(int, int));
 
   /*----------------------------------------------------------------------------
-
+  Recibe dos números, si a > b devuelve 1. Devuelve 0 en caso contrario.
   ----------------------------------------------------------------------------*/
   int esMayor(int a, int b);
 
   /*----------------------------------------------------------------------------
-
+  Recibe dos números, si a < b devuelve 1. Devuelve 0 en caso contrario.
   ----------------------------------------------------------------------------*/
   int esMenor(int a, int b);
 
   /*----------------------------------------------------------------------------
-
+  Imprime por pantalla el vector de estructuras, indicando la posición de cada
+  struct y los 3 campos de cada uno.
   ----------------------------------------------------------------------------*/
   void printArray (ficha *vector, int nEle);
 
