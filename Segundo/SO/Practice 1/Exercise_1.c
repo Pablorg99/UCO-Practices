32ba3d5f18f83cc4d49086c24ca60edeedd379e2
Pablorg99 <i72rogup@uco.es>
2018-09-27 10:56:11 +0200

    Ex 1, comments and others

--- Segundo/SO/Practice 1/Exercise_1.c
+++ Segundo/SO/Practice 1/Exercise_1.c
@@ -1,11 +1,14 @@
-//ftok()
-#include <sys/ipc.h>
-//key_t and pid_t
+/*------------------------------------------------------------------------------
+Exercise_1.c
+In this file, a parent process creates 3 different child processes that print
+their own process id (PID). Then the parent process wait until the children end
+and print the PID of the process that has finished and its status.
+------------------------------------------------------------------------------*/
+
+//key_t
 #include <sys/types.h>
 //fork() and getpid()
 #include <unistd.h>
-//shared memory functions
-#include <sys/shm.h>
 //exit()
 #include <stdlib.h>
 //wait()
@@ -15,7 +18,8 @@
 
 int main() {
 
-  int shmid, status;
+  //Status store the value returned from exit() in wait() (see parent waiting)
+  int status;
   key_t key;
 
     if (! fork()) {
@@ -33,6 +37,7 @@ int main() {
       exit(0);
     }
 
+    //Parent process wait for children
     for (int i = 0; i < 3; i++) {
       pid_t pid = wait(&status);
       printf("Child with PID = %d finished with status: %d\n", pid, WEXITSTATUS(status));
