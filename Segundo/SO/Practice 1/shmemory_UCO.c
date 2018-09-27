3a9cd6e6e3a0dcfb0b31dccc175a51d64b1fe7b7
Pablorg99 <i72rogup@uco.es>
2018-09-27 10:06:24 +0200

    Idents

--- Segundo/SO/Practice 1/shmemory_UCO.c
+++ Segundo/SO/Practice 1/shmemory_UCO.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <math.h>
 
 #define ITER		5
 #define CHILDREN	5
@@ -17,68 +16,68 @@
 
 int main()
 {
-    int i, shmid, status;
-    key_t key;
-    double *counter;
-    void adder(int);
-    struct shmid_ds buf;
+  int i, shmid, status;
+  key_t key;
+  double *counter;
+  void adder(int);
+  struct shmid_ds buf;
 
-    // Shared memory
-    key = ftok("shmemory.c", 1);
-    if ((shmid = shmget(key, sizeof(double), IPC_CREAT | 0777)) == -1)
-	     exit(1);
+  // Shared memory
+  key = ftok("shmemory.c", 1);
+  if ((shmid = shmget(key, sizeof(double), IPC_CREAT | 0777)) == -1)
+     exit(1);
 
-    counter = (double *) shmat(shmid, NULL, 0);
-    *counter = 0.0;
+  counter = (double *) shmat(shmid, NULL, 0);
+  *counter = 0.0;
 
-    // Run children
-    for (i = 0; i < CHILDREN; i++) {
-    	if (!fork()) {
-    	    adder(i);
-    	    exit(0);
-	    }
+  // Run children
+  for (i = 0; i < CHILDREN; i++) {
+  	if (!fork()) {
+  	    adder(i);
+  	    exit(0);
     }
+  }
 
-    // Wait to finish
-    for (i = 0; i < CHILDREN; i++) {
-    	pid_t pid = wait(&status);
-    	printf("\nChild %d finished with status %d\n", pid, WEXITSTATUS(status));
-    }
+  // Wait to finish
+  for (i = 0; i < CHILDREN; i++) {
+  	pid_t pid = wait(&status);
+  	printf("\nChild %d finished with status %d\n", pid, WEXITSTATUS(status));
+  }
 
-    // Final result
-    fprintf(stdout, "Counter: %f\n", *counter);
+  // Final result
+  fprintf(stdout, "Counter: %f\n", *counter);
 
-    // Detach segment
-    shmdt(counter);
+  // Detach segment
+  shmdt(counter);
 
-    // Mark the segment to be destroyed
-    shmctl(shmid, IPC_RMID, &buf);
+  // Mark the segment to be destroyed
+  shmctl(shmid, IPC_RMID, &buf);
 
-    return 0;
+  return 0;
 }
 
 void adder(int id)
 {
-    double x = 1.0, l;
-    double *counter;
-    int i, shmid;
-    key_t key;
+  double x = 1.0, l;
+  double *counter;
+  int i, shmid;
+  key_t key;
 
-    // Shared memory
-    key = ftok("shmemory.c", 1);
-    if ((shmid = shmget(key, sizeof(double), 0)) == -1)
-	     perror("Child: ");
-    counter = (double *) shmat(shmid, NULL, 0);
+  // Shared memory
+  key = ftok("shmemory.c", 1);
+  if ((shmid = shmget(key, sizeof(double), 0)) == -1)
+     perror("Child: ");
+  counter = (double *) shmat(shmid, NULL, 0);
 
-    for (i = 0; i < ITER; i++) {
-    	l = *counter;
-    	fprintf(stdout, "Process %d: %lf\n", id, *counter);
-    	l += x;
-    	*counter = l;
-    }
-    fprintf(stdout, "*Process %d: %lf\n", id, *counter);
-    shmdt(counter);
+  for (i = 0; i < ITER; i++) {
+  	l = *counter;
+  	fprintf(stdout, "Process %d: %lf\n", id, *counter);
+  	l += x;
+  	*counter = l;
+  }
+  fprintf(stdout, "*Process %d: %lf\n", id, *counter);
+  shmdt(counter);
 
 
-    exit(Ok);
+  exit(Ok);
 }
