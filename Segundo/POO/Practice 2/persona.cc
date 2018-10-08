cdc05856f939829b39dd076a668c1db61c7f7122
Pablorg99 <i72rogup@uco.es>
2018-10-08 19:44:37 +0200

    Class 'Persona' from p2 oop added and tested (add some comments)

---
+++ Segundo/POO/Practice 2/persona.cc
@@ -0,0 +1,35 @@
+#include <string>
+#include "persona.h"
+using std::string;
+
+Persona::Persona (string dni, string name, string surnames,
+int age, string address, string locality,
+string province, string country) {
+  dni_ = dni;
+  name_ = name;
+  surnames_ = surnames;
+  age_ = age;
+  address_ = address;
+  locality_ = locality;
+  province_ = province;
+  country_ = country;
+}
+
+string Persona::getApellidosyNombre() const {
+  string full_name;
+  full_name = surnames_ + ", " + name_;
+  return full_name;
+}
+
+bool Persona::setEdad(int age) {
+  if (age < 0 || age > 140) return false;
+  else {
+    age_ = age;
+    return true;
+  }
+}
+
+bool Persona::mayor() const {
+  if (age_ >= 18) return true;
+  else return false;
+}
---
+++ Segundo/POO/Practice 2/persona.h
@@ -0,0 +1,47 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <string>
+using std::string;
+
+
+class Persona {
+  private:
+    string dni_;
+    string name_;
+    string surnames_;
+    int age_;
+    string address_;
+    string locality_;
+    string province_;
+    string country_;
+  public:
+    //Constructor
+    Persona (string dni, string name = "", string surnames = "",
+    int edad = 0, string address = "", string locality = "",
+    string province = "", string country = "");
+    //Getter methods
+    string getDNI() const {return dni_;}
+    string getNombre() const {return name_;}
+    string getApellidos() const {return surnames_;}
+    int getEdad() const {return age_;}
+    string getDireccion() const {return address_ ;}
+    string getLocalidad() const {return locality_;}
+    string getProvincia() const {return province_;}
+    string getPais() const {return country_;}
+    //concatenates name and surnames with format "surnames, name"
+    string getApellidosyNombre() const;
+    //Setter methods
+    void setDNI(string dni) {dni_ = dni;}
+    void setNombre(string name) {name_ = name;}
+    void setApellidos(string surnames) {surnames_ = surnames;}
+    bool setEdad(int age);
+    void setDireccion(string address) {address_ = address;}
+    void setLocalidad(string locality) {locality_ = locality;}
+    void setProvincia(string province) {province_ = province;}
+    void setPais(string country) {country_ = country;}
+    //Other methods
+    bool mayor() const;
+};
+
+#endif
