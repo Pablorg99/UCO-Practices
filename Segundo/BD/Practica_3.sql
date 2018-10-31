a990c29f618fc7a1a65772a40347e9eead3cd036
Pablorg99 <i72rogup@uco.es>
2018-10-31 15:07:01 +0100

    P3 finished

--- Segundo/BD/Practica_3.sql
+++ Segundo/BD/Practica_3.sql
@@ -1,1 +1,44 @@
--- Practica_3
+-- Ejercicio 1
+SELECT v.nombrecompleto FROM votantes v, localidades l 
+    WHERE SUBSTR(v.dni, 8,8) = (l.idlocalidad + 1) AND l.idlocalidad = v.localidad;
+
+-- Ejercicio 2
+SELECT v.nombrecompleto, DECODE(l.idlocalidad, 1, 'Madrid', 2, 'Madrid', 3, 'Madrid', l.nombre) as LOCALIDAD FROM votantes v, localidades l
+    WHERE l.idlocalidad = v.localidad;
+
+-- Ejercicio 3
+SELECT p.siglas FROM partidos p, eventos_resultados er
+    WHERE p.idpartido = er.partido 
+    HAVING COUNT(er.partido) = (SELECT MAX(COUNT(partido)) FROM eventos_resultados GROUP BY partido)
+    GROUP BY p.siglas;
+
+-- Ejercicio 4
+SELECT dni FROM votantes
+    WHERE fechanacimiento = (SELECT MIN(fechanacimiento) FROM votantes
+                                WHERE fechanacimiento > (SELECT MIN(fechanacimiento) FROM votantes));
+
+-- Ejercicio 5
+SELECT v.dni, COUNT(c.votante) FROM votantes v, consultas c
+    WHERE v.dni = c.votante
+    GROUP BY v.dni
+    ORDER BY COUNT(c.votante) DESC;
+
+-- Ejercicio 6
+SELECT v.dni, COUNT(c.votante) FROM votantes v, consultas c
+    WHERE v.dni = c.votante
+    HAVING COUNT(c.votante) > ((SELECT COUNT(idconsulta) FROM consultas) / (SELECT COUNT(dni) FROM votantes))
+    GROUP BY v.dni
+    ORDER BY COUNT(c.votante) DESC;
+    
+-- Ejercicio 7
+SELECT v.nombrecompleto FROM votantes v, consultas c
+    WHERE v.dni = c.votante
+    HAVING COUNT(c.votante) > ((SELECT COUNT(idconsulta) FROM consultas) / (SELECT COUNT(dni) FROM votantes))
+    GROUP BY v.nombrecompleto;
+    
+-- Ejercicio 8
+SELECT v.dni, COUNT(c.votante) FROM votantes v, consultas c
+    WHERE v.dni = c.votante AND v.fechanacimiento != (SELECT MIN(fechanacimiento) FROM votantes
+                                                            WHERE fechanacimiento > (SELECT MIN(fechanacimiento) FROM votantes))
+    GROUP BY v.dni
+    ORDER BY COUNT(c.votante) DESC;
