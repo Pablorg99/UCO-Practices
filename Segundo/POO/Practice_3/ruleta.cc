7c41fdcb14fa4727746318a0fc8e3213cb8dfa54
Pablorg99 <i72rogup@uco.es>
2018-10-31 12:15:26 +0100

    P3 deleteJugador y escribeJugadores methods added

--- Segundo/POO/Practice_3/ruleta.cc
+++ Segundo/POO/Practice_3/ruleta.cc
@@ -25,30 +25,45 @@ bool Ruleta::setBola(const int number) {
 }
 
 bool Ruleta::addJugador(Jugador player) {
-
     if(playerInList_(player)) return false;
-       
     else {
         players_.push_back(player);
         string name = player.getDNI() + ".txt";
-        //If DNI.txt does not exists, create it
         //DNI is the dni from the player passed as parameter
+        //If DNI.txt does not exists, create it
         if(!ifstream(name)) ofstream(name);
         return true;
     }
 }
 
 int Ruleta::deleteJugador(Jugador player) {
+    if (players_.empty()) return -2;
+    else if (playerInList_(player)) {
+        players_.remove(player);
+        return 1;
+    }
+    else return -1;
+}
+
+void Ruleta::escribeJugadores() {
+    ofstream pfile("jugadores.txt");
+    list <Jugador> :: iterator it;
     
+    for(it = players_.begin(); it != players_.end() ; it++) {
+        pfile << it->getDNI() << "," << it->getCodigo() << "," << it->getNombre() << "," << 
+        it->getApellidos() << "," << it->getDireccion() << "," << it->getLocalidad() << ","
+        << it->getProvincia() << "," << it->getPais() << "," << it->getDinero() << std::endl;
+    }
+    
+    pfile.close();
 }
 
 bool Ruleta::playerInList_(Jugador player) {
     list <Jugador> :: iterator it;
     //go all over the list checking if dni from the player
     //passed as parameter is in the list players_
-    for(it = players_.begin(); it != players_.end(); it++) 
-    {        
-        if ((*it).getDNI() == player.getDNI()) return true;
+    for(it = players_.begin(); it != players_.end(); it++) {        
+        if (it->getDNI() == player.getDNI()) return true;
     }
     return false;
 }
