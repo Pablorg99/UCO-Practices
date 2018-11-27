2d62cc41577bef1a2c773609871e93234d519c45
Pablorg99 <i72rogup@uco.es>
2018-11-27 21:08:06 +0100

    Advances in Contador class

---
+++ Segundo/POO/Practice_4/contador.cc
@@ -0,0 +1,21 @@
+#include "contador.h"
+
+Contador::Contador(int value = 0, int min = 0, int max = 1000) {
+    if(value <= min || value >= max || min > max) {
+        value_ = 0;
+        min_ = 0;
+        max_ = 1000;
+    }
+    else {
+        value_ = value;
+        min_ = min;
+        max_ = max;
+    }
+}
+
+Contador Contador::operator=(int integer) {
+    if (integer < min_) value_ = min_;
+    else if (integer > max_) value_ = max_;
+    else value_ = integer;
+    return *this;
+}
--- Segundo/POO/Practice_4/contador.h
+++ Segundo/POO/Practice_4/contador.h
@@ -7,6 +7,23 @@ class Contador {
         int min_;
         int max_;
     private:
+        //Constructor
+        /*If there are logical errors like: value lower or higher than min or max
+        or min higher than max, Contador is set with default values (0, 0, 1000*/ 
+        Contador(int value = 0, int min = 0, int max = 1000);
+        //Getter
+        inline int get() {return value_;}
+        //Called when Contador c = integer
+        /*Returns the Contador object who called the function and
+        assign integer to value_ if the integer is betweem min and max*/
+        Contador operator=(int integer);
+        //Contador c = Contador d is not necessary to do, because is the default = operator
+        //Unary Operators
+        //NEED TO FIX (check_?)
+        inline Contador operator++(void) {++value_; return *this;}
+        inline Contador operator++(int) {++value_; return *this;}
+        inline Contador operator--(void) {--value_; return *this;}
+        inline Contador operator--(int) {--value_; return *this;}
 };
 
 #endif
