f2d6852171e44c393a29c93089c73cc1bc2fedae
Pablorg99 <i72rogup@uco.es>
2018-12-24 10:09:14 +0100

    Contador class almost finished

--- Segundo/POO/Practice_4/contador.cc
+++ Segundo/POO/Practice_4/contador.cc
@@ -1,7 +1,13 @@
+/*-----------------------------------------------------------------------------------------------------
+Pablo Rodríguez Guillén
+contador.cc
+File with the implementation of the different methods of the class Contador.
+-----------------------------------------------------------------------------------------------------*/
+
 #include "contador.h"
 
 Contador::Contador(int value = 0, int min = 0, int max = 1000) {
-    if(value <= min || value >= max || min > max) {
+    if(value < min || value > max || min > max) {
         value_ = 0;
         min_ = 0;
         max_ = 1000;
@@ -21,18 +27,54 @@ Contador Contador::operator=(int integer) {
 }
 
 Contador Contador::operator++(void) {
-	if((value_ + 1) > max) return *this; 
+	if((this->get() + 1) > max_) return *this; 
 	else {
 		++value_;
 		return *this;
 	}
 }
 Contador Contador::operator++(int) {
-	++value_; return *this;
+	Contador temp = *this;
+	if((this->get() + 1) > max_) return temp; 
+	else {
+		++value_;
+		return temp;
+	}
 }
 Contador Contador::operator--(void) {
-	--value_; return *this;
+	if((this->get() - 1) < min_) return *this; 
+	else {
+		--value_;
+		return *this;
+	}
 }
 Contador Contador::operator--(int) {
-	--value_; return *this;
+	Contador temp = *this;
+	if((this->get() - 1) < min_) return temp; 
+	else {
+		++value_;
+		return temp;
+	}
+}
+
+Contador Contador::operator+(int addition) {
+	if((this->get() + addition) > max_) value_ = max_;
+	else value_ += addition;
+	return *this;
+}
+Contador Contador::operator-(int substraction) {
+	if((this->get() -substraction) < min_) value_ = min_;
+	else value_ -= substraction;
+	return *this;
+}
+
+Contador operator+(int addition, Contador &counter) {
+	if((counter.get() + addition) > counter.max_) counter.value_ = counter.max_;
+	else counter.value_ += addition;
+	return counter;
+}
+Contador operator-(int substraction, Contador &counter) {
+	if((counter.get() - substraction) > counter.min_) counter.value_ = counter.min_;
+	else counter.value_ += substraction;
+	return counter;
 }
--- Segundo/POO/Practice_4/contador.h
+++ Segundo/POO/Practice_4/contador.h
@@ -1,3 +1,9 @@
+/*-----------------------------------------------------------------------------------------------------
+Pablo Rodríguez Guillén
+contador.h
+Header file of the class Contador and its methods
+-----------------------------------------------------------------------------------------------------*/
+
 #ifndef CONTADOR_H
 #define CONTADOR_H
 
@@ -9,7 +15,7 @@ class Contador {
     private:
         //Constructor
         /*If there are logical errors like: value lower or higher than min or max
-        or min higher than max, Contador is set with default values (0, 0, 1000*/ 
+        or min higher than max, Contador is set with default values (0, 0, 1000)*/ 
         Contador(int value = 0, int min = 0, int max = 1000);
         //Getter
         inline int get() {return value_;}
@@ -19,11 +25,17 @@ class Contador {
         Contador operator=(int integer);
         //Contador c = Contador d is not necessary to do, because is the default = operator
         //Unary Operators
-        //NEED TO FIX (check_?)
-        inline Contador operator++(void);
-        inline Contador operator++(int);
-        inline Contador operator--(void);
-        inline Contador operator--(int);
+        Contador operator++(void); //++c
+        Contador operator++(int); //c++
+        Contador operator--(void); //--c
+        Contador operator--(int); //c--
+		//Binary Operators (counter +/- integer)
+		Contador operator+(int addition);
+		Contador operator-(int substraction);
+		/*Binary operators like: integer +/- counter, should be declared as non-member
+		functions of the class, I declare them as friend to be able to use the get() method*/
+		friend Contador operator+(int addition, Contador &counter);
+		friend Contador operator-(int substraction, Contador &counter);
 };
 
 #endif
