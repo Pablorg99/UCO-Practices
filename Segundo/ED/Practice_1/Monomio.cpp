22d3186211cd84feb5713d16965c4fee980edc9f
Pablorg99 <i72rogup@uco.es>
2019-02-17 12:39:37 +0100

    First part of the practice done

--- Segundo/ED/Practice_1/Monomio.cpp
+++ Segundo/ED/Practice_1/Monomio.cpp
@@ -7,13 +7,25 @@
 
 //  Ficheros de cabecera
 #include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+using std::to_string;
 
 #include "Monomio.hpp"
 
-ed::Monomio::Monomio(float coeficiente, int grado) {
+//Constructores
+
+ed::Monomio::Monomio(double coeficiente, int grado) {
 	setCoeficiente(coeficiente);
 	setGrado(grado);
 }
+
+///////////////////////////////////////////////////////////////////////////////////
+
+//Operadores de asignación
 
 /*
 ed::Monomio & ed::Monomio::operator=(ed::Monomio const &m)
@@ -24,12 +36,16 @@ ed::Monomio & ed::Monomio::operator=(ed::Monomio const &m)
 }
 */
 
-ed::Monomio ed::Monomio::operator=(float const numero_real) {
+ed::Monomio ed::Monomio::operator=(double const numero_real) {
 	setGrado(0);
 	setCoeficiente(numero_real);
 	// Se devuelve el objeto actual
 	return *this;
 }
+
+///////////////////////////////////////////////////////////////////////////////////
+
+//Operadores aritmeticos y de asignación entre monomios
 
 ed::Monomio ed::Monomio::operator+=(Monomio const &monomio) {
 	if(monomio.getGrado() == this->getGrado()) {
@@ -39,20 +55,98 @@ ed::Monomio ed::Monomio::operator+=(Monomio const &monomio) {
 	return *this;
 }
 
-// COMPLETAR EL RESTO DE OPERADORES
+ed::Monomio ed::Monomio::operator-=(Monomio const &monomio) {
+	if(monomio.getGrado() == this->getGrado()) {
+		this->setCoeficiente(this->getCoeficiente() - monomio.getCoeficiente());
+	}
+	//Se devuelve el objeto actual
+	return *this;
+}
 
+ed::Monomio ed::Monomio::operator*=(Monomio const &monomio) {
+	this->setCoeficiente(this->getCoeficiente() * monomio.getCoeficiente());
+	this->setGrado(this->getGrado() + monomio.getGrado());
+	//Se devuelve el objeto actual
+	return *this;
+}
 
+ed::Monomio ed::Monomio::operator/=(Monomio const &monomio) {
+	if((monomio.getGrado() <= this->getGrado()) && !((abs(monomio.getCoeficiente() - 0.0)) < COTA_ERROR)) {
+		this->setGrado(this->getGrado() - monomio.getGrado());
+		this->setCoeficiente(this->getCoeficiente() / monomio.getCoeficiente());
+	}
+	//Se devuelve el objeto actual
+	return *this;
+}
 
-///////////////////////////////////////////////////////////////////////
+//Operadores aritmeticos y de asignación con reales
+
+ed::Monomio ed::Monomio::operator*=(double numero_real) {
+	setCoeficiente(getCoeficiente() * numero_real);
+	//Se devuelve el objeto actual
+	return *this;
+}
+
+ed::Monomio ed::Monomio::operator/=(double numero_real) {
+	if(!((abs(numero_real - 0.0)) < COTA_ERROR)) {
+		setCoeficiente(getCoeficiente() / numero_real);
+	}
+	//Se devuelve el objeto actual
+	return *this;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
 
 // Funciones lectura y escritura de la clase Monomio
 
-// COMPLETAR
+void ed::Monomio::leerMonomio() {
+	double coeficiente_auxiliar;
+	int grado_auxiliar;
 
+	cout << "Introduce los valores del monomio:" << endl;
+	cout << "\tCoeficiente: ";
+	cin >> coeficiente_auxiliar;
+	cout << "\tGrado (mayor o igual a 0): ";
+	cin >> grado_auxiliar;
+	//Se asegura de que el grado introducido no sea negativo
+	while(grado_auxiliar < 0){
+		cout << "Introduzca un grado mayor o igual a 0: ";
+		cin >> grado_auxiliar;
+	}
 
-///////////////////////////////////////////////////////////////////////
+	setCoeficiente(coeficiente_auxiliar);
+	setGrado(grado_auxiliar);
+}
+
+void ed::Monomio::escribirMonomio() {
+	string coeficiente;
+	string grado;
+
+	//Formateo excepcional
+	if((getGrado() == 0) && ((abs(getCoeficiente() - (1.0)) < COTA_ERROR))) cout << "Monomio: 1" << endl;
+	else if((getGrado() == 0) && ((abs(getCoeficiente() - (-1.0)) < COTA_ERROR))) cout << "Monomio: -1" << endl;
+	else {
+		//Formateo del coeficiente
+		if((abs(getCoeficiente() - (1.0)) < COTA_ERROR)) coeficiente = "";
+		else if((abs(getCoeficiente() - (-1.0)) < COTA_ERROR)) coeficiente = "-";
+		else coeficiente = to_string(getCoeficiente());
+		//Formateo del grado	
+		if(getGrado() == 0) grado = "";
+		else if(getGrado() == 1) grado = "X";
+		else grado = "X^" + to_string(getGrado());
+
+		cout << "Monomio: " << coeficiente << grado << endl;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////
 
 // Funciones auxiliares de la clase Monomio
+double ed::Monomio::calcularValor(double numero_real) {
+	double resultado;
 
-// COMPLETAR
+	resultado = getCoeficiente() * pow(numero_real, getGrado());
+
+	return resultado;
+}
 
--- Segundo/ED/Practice_1/Monomio.hpp
+++ Segundo/ED/Practice_1/Monomio.hpp
@@ -26,7 +26,7 @@ namespace ed
 	{
 		//!\name Atributos privados de la clase Monomio
 		private:
-			float coeficiente_; //!< Coeficiente del monomio
+			double coeficiente_; //!< Coeficiente del monomio
 			int grado_; //!< Grado del monomio
 		
 		//! \name Metodos publicos de la clase Monomio
@@ -42,17 +42,18 @@ namespace ed
 				\post Se establecen valores para el coeficiente y el grado del monomio
 				\sa setCoeficiente(), setGrado()
 			*/
-			Monomio(float coeficiente = 0.0, int grado = 0);
+			Monomio(double coeficiente = 0.0, int grado = 0);
 
 			//! \name Observadores: funciones de consulta de la clase Monomio
 
 			/*!
 				\brief Observador del coeficiente del monomio
 				\note Funcion inline
-				\return Coeficiente del monomio (float)
+				\return Coeficiente del monomio (double)
 				\sa getGrado
 			*/
-			inline float getCoeficiente() const {return coeficiente_;}
+			inline double getCoeficiente() const {return coeficiente_;}
+
 			/*!
 				\brief Observador del grado del monomio
 				\note Funcion inline
@@ -69,7 +70,8 @@ namespace ed
 				\param new_coeficiente: Coeficiente a establecer
 				\sa setGrado
 			*/
-			inline void setCoeficiente(float new_coeficiente) {coeficiente_ = new_coeficiente;}
+			inline void setCoeficiente(double new_coeficiente) {coeficiente_ = new_coeficiente;}
+
 			/*!
 				\brief Modifica el grado del monomio que invoca el método
 				\note Funcion inline
@@ -85,31 +87,99 @@ namespace ed
 			/*!
 				\breif 
 			*/
-			Monomio & operator=(Monomio const &m);
+			//Monomio & operator=(Monomio const &m);
 
 			/*!
 				\brief Establece el grado a 0 y el coeficiente al flotante al que se iguala el monomio
 				\param numero_real: Nuevo coeficiente del monomio
 				\return Monomio sometido a la operacion
 			*/
-			Monomio Monomio::operator=(float const numero_real);
+			Monomio Monomio::operator=(double const numero_real);
 
 			/*!
-				\brief 
+				\brief Implementacion del operador += para 2 monomios
+				\param monomio: Referencia al monomio que se suma al monomio que invoca el operador
+				\pre Ambos monomios tienen el mismo grado
+				\return Monomio resultante de la operacion
+				\post El monomio devuelto tiene como coeficiente la suma de los coeficientes de ambos monomios y el grado comun a ambos
 			*/
 			Monomio Monomio::operator+=(Monomio const &monomio);
 
+			/*!
+				\brief Implementacion del operando -= para 2 monomios
+				\param monomio: Referencia al monomio que se resta al que invoca el operador
+				\pre Ambos monomios tienen el mismo grado
+				\return Monomio resultante de la operacion
+				\post El coeficiente del monomio devuelto es la resta de ambos coeficientes. El grado es el comun a ambos
+			*/
+			Monomio Monomio::operator-=(Monomio const &monomio);
+
+			/*!
+				\brief Implementacion del operador *= para 2 monomios
+				\param monomio: Referencia al monomio que multiplica al que invoca el operador
+				\return Monomio resultante de la operacion
+				\post El coeficiente del monomio original se multiplica con el del pasado por referencia. 
+					  El grado resultante es la suma del grado de ambos.
+			*/
+			Monomio Monomio::operator*=(Monomio const &monomio);
+
+			/*!
+				\brief Implementacion del operador /= para 2 monomios
+				\note Funcion sobrecargada
+				\param monomio: Referencia al monomio que divide al que invoca el operador
+				\pre El grado del monomio pasado por referencia es menor o igual que el del monomio actual
+					 El coeficiente del monomio pasado por referencia no es 0
+				\return Monomio resultante de la operacion
+				\post El coeficiente del monomio original se divide entre el del pasado por referencia. 
+					  El grado resultante es la resta del grado de ambos. 
+			*/
+			Monomio Monomio::operator/=(Monomio const &monomio);
+
+			/*!
+				\brief Implementacion del operador *= de un monomio con un real
+				\note Funcion sobrecargada
+				\param numero_real: Numero que multiplica al monomio actual
+				\return monomio resultante de la operacion
+				\post El coeficiente del monomio se multiplica por el numero pasado mientras que el grado permanece invariado
+			*/
+			Monomio Monomio::operator*=(double numero_real);
+
+			/*!
+				\brief Implementacion del operador /= de un monomio con un real
+				\note Funcion sobrecargada
+				\param numero_real: Numero que divide al monomio actual
+				\pre El numero real no es 0.0
+				\return monomio resultante de la operacion
+				\post El coeficiente del monomio se divide entre el numero pasado mientras que el grado permanece invariado
+			*/
+			Monomio Monomio::operator/=(double numero_real);
+
 			//! \name Funciones lectura y escritura de la clase Monomio
-			/*!
-
-			*/
 
 			/*!
-
+				\brief Pide al usuario que introduce los valores del monomio por teclado
+				\post El grado es mayor o igual que 0
 			*/
-	
-			//! \name Funciones auxiliares de la clase Monomio
 			
+			void leerMonomio();
+			/*!
+				\brief Imprime el monomio por pantalla con un formato
+				\note Formato: coeficiente X^grado
+					  Coeficiente 1: X^grado
+					  Coeficiente -1: -X^grado
+					  Grado 0: Coeficiente
+					  Grado 1: X (sin grado)
+			*/
+			void escribirMonomio();
+
+			//! \name Funciones auxiliares de la clase Monomio
+
+			/*!
+				\brief Calcula el valor del monomio para un valor de X
+				\param numero_real: Numero por el que sustituir la X para operar
+				\return El numero real que resulta de operar el monomio
+			*/
+			double calcularValor(double numero_real);
 	}; // Fin de la definición de la clase Monomio
 
 } //  Fin de namespace ed.
