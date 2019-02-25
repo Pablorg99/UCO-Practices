944b9577c4542cd749d5c1ac33feae29d307acde
Pablorg99 <i72rogup@uco.es>
2019-02-25 23:57:33 +0100

    Operadores Externos terminados

--- Segundo/ED/Practice_1/operadoresExternosMonomios.cpp
+++ Segundo/ED/Practice_1/operadoresExternosMonomios.cpp
@@ -17,10 +17,10 @@ namespace ed
 {
 	// Operadores de igualdad
 	bool operator==(ed::Monomio const & monomio_1, ed::Monomio const & monomio_2) {
-		if(monomio_1.getGrado() == monomio_2.getGrado() && ((abs(monomio_1.getCoeficiente() - monomio_2.getCoeficiente())) < COTA_ERROR))
-		{
-		return true;
-		}
+		//Booleanos locales para ver si se cumplen igualdades
+		bool equalGrado = monomio_1.getGrado() == monomio_2.getGrado();
+		bool equalCoeficiente = (abs(monomio_1.getCoeficiente() - monomio_2.getCoeficiente()) < COTA_ERROR);
+		if(equalGrado && equalCoeficiente)	return true;
 		else return false;
 	}
 
@@ -30,101 +30,137 @@ namespace ed
 	}
 
 	bool operator==(double numero_real, ed::Monomio const & monomio) {
-		if(monomio.getGrado() == 0 && (abs(monomio.getCoeficiente() - numero_real) < COTA_ERROR)) return true;
+		if(monomio == numero_real) return true;
 		else return false;
 	}
 
 	// Operadores de desigualdad
-	bool operator!=(ed::Monomio const & monomio_1, ed::Monomio const & monomio_2) {
-		if(monomio_1.getGrado() == monomio_2.getGrado() && ((abs(monomio_1.getCoeficiente() - monomio_2.getCoeficiente())) < COTA_ERROR))
-		{
-		return false;
-		}
+	bool operator!=(ed::Monomio const & monomio_1, Monomio const & monomio_2) {
+		if(monomio_1 == monomio_2) return false;
 		else return true;
 	}
 
 	bool operator!=(ed::Monomio const & monomio, double numero_real) {
-		if(monomio.getGrado() == 0 && (abs(monomio.getCoeficiente() - numero_real) < COTA_ERROR)) return false;
+		if(monomio == numero_real) return false;
 		else return true;
 	}
 
 	bool operator!=(double numero_real, ed::Monomio const & monomio) {
-		if(monomio.getGrado() == 0 && (abs(monomio.getCoeficiente() - numero_real) < COTA_ERROR)) return false;
-		else return true;
+		if(monomio != numero_real) return true;
+		else return false;
 	}
-
-	////////////////////////////////////////////////////////////
 
 	// Operadores unarios prefijos 
 
-	// COMPLETAR
-	ed::Monomio & operator+(ed::Monomio const & m)
-	{
-		// COMPLETAR Y MODIFICAR
-		// Se crea un nuevo objeto
-		ed::Monomio *nuevo = new ed::Monomio();
-
-
-		// Se devuelve el resultado
-		return *nuevo;
+	Monomio & operator+(Monomio const & monomio) {
+		Monomio *new_monomio = new Monomio(monomio.getCoeficiente(), monomio.getGrado());
+		return *new_monomio;
 	}
 
-	// COMPLETAR EL OTRO OPERADOR UNARIO PREFIJO: resta
+	Monomio & operator-(Monomio const & monomio) {
+		Monomio *new_monomio = new Monomio((-1 * monomio.getCoeficiente()), monomio.getGrado());
+		return *new_monomio;
+	}
 
-
-	////////////////////////////////////////////////////////////
 	// Operadores aritméticos binarios
 
 	// Suma
-	ed::Monomio & operator+ (ed::Monomio const &m1, ed::Monomio const &m2)
-	{
-		// COMPLETAR Y MODIFICAR
+	Monomio & operator+(Monomio const &monomio_1, Monomio const &monomio_2) {
+		#ifndef NDEBUG
+			assert(monomio_1.getGrado() != monomio_2.getGrado());
+		#endif
 		// Se crea un nuevo objeto
-		ed::Monomio *nuevo = new ed::Monomio();
-
-	
+		Monomio *new_monomio = new Monomio(monomio_1.getCoeficiente() + monomio_2.getCoeficiente(), monomio_1.getGrado());
 		// Se devuelve el resultado
-		return *nuevo;
+		return *new_monomio;
 	}
 
-
-	////////////
 	// Resta
+	Monomio & operator-(Monomio const &monomio_1, Monomio const &monomio_2) {
+		#ifndef NDEBUG
+			assert(monomio_1.getGrado() != monomio_2.getGrado());
+		#endif
+		// Se crea un nuevo objeto
+		Monomio *new_monomio = new Monomio(monomio_1.getCoeficiente() - monomio_2.getCoeficiente(), monomio_1.getGrado());
+		// Se devuelve el resultado
+		return *new_monomio;
+	}
 
-	// COMPLETAR
-
-
-	//////////////////
 	// Multiplicación
+	Monomio operator*(Monomio const &monomio_1, Monomio const &monomio_2) {
+		int new_grado = monomio_1.getGrado() + monomio_2.getGrado();
+		double new_coeficiente = monomio_1.getCoeficiente() * monomio_2.getCoeficiente();
+		//Se crea un nuevo objeto
+		Monomio *new_monomio = new Monomio(new_coeficiente, new_grado);
+		//Se devuelve el resultado
+		return *new_monomio;
+	}
 
-	// COMPLETAR
-
-	////////////
+	Monomio & operator*(Monomio const &monomio, double numero_real) {
+		//Se crea un nuevo objeto
+		Monomio *new_monomio = new Monomio(monomio.getCoeficiente() * numero_real, monomio.getGrado());
+		//Se devuelve el resultado
+		return *new_monomio;
+	}
+	
+	Monomio & operator*(double numero_real, Monomio const &monomio) {
+		//Se crea un nuevo objeto
+		Monomio *new_monomio = &(monomio * numero_real);
+		//Se devuelve el resultado
+		return *new_monomio;
+	}
+	
 	// División
+	Monomio & operator/(Monomio const &monomio_1, Monomio const &monomio_2) {
+		#ifndef NDEBUG
+			assert(monomio_1.getGrado() < monomio_2.getGrado());
+			assert(abs(monomio_2.getCoeficiente() - 0.0) < COTA_ERROR);
+		#endif
+		int new_grado = monomio_1.getGrado() - monomio_2.getGrado();
+		double new_coeficiente = monomio_1.getCoeficiente() / monomio_2.getCoeficiente();
+		//Se crea un nuevo objeto
+		Monomio *new_monomio = new Monomio(new_coeficiente, new_grado);
+		//Se devuelve el resultado
+		return *new_monomio;
+	}
 
-	// COMPLETAR
+	Monomio & operator/(double numero_real, Monomio const &monomio) {
+		#ifndef NDEBUG
+			assert(abs(monomio.getCoeficiente() - 0.0) < COTA_ERROR);
+			assert(monomio.getGrado() != 0);
+		#endif
+		//Se crea un nuevo objeto
+		Monomio *new_monomio = new Monomio(numero_real / monomio.getCoeficiente(), monomio.getGrado());
+		//Se devuelve el resultado
+		return *new_monomio;
+	}
 
-
-	/////////////////////////////////////////////////////////////////////////////
+	Monomio & operator/(Monomio const &monomio, double numero_real) {
+		#ifndef NDEBUG
+			assert(numero_real == 0);
+		#endif
+		//Se crea un nuevo objeto
+		Monomio *new_monomio = new Monomio(monomio.getCoeficiente() / numero_real, monomio.getGrado());
+		//Se devuelve el resultado
+		return *new_monomio;
+	}
 
 	//  Sobrecarga del operador de entrada
-	istream &operator>>(istream &stream, ed::Monomio &m) 
-	{
-		// COMPLETAR
-
+	istream &operator>>(istream &stream, Monomio &monomio) {
+		string cadena_auxiliar;
+		getline(stream, cadena_auxiliar, ' ');
+		monomio.setCoeficiente(atof(cadena_auxiliar.c_str()));
+		getline(stream, cadena_auxiliar); //El valor por defecto de separacion es \n
+		monomio.setGrado(atoi(cadena_auxiliar.c_str()));
    		// Se devuelve el flujo de entrada
 		return stream;
 	}
 
 
 	//  Sobrecarga del operador de salida
-	ostream &operator<<(ostream &stream, ed::Monomio const &m) 
-	{
-		// COMPLETAR
-
+	ostream &operator<<(ostream &stream, Monomio const &monomio) {
+		stream << monomio.getCoeficiente() << " " << monomio.getGrado();
 		// Se devuelve el flujo de salida
 		return stream;
 	}
-
-
-}  // namespace ed
+} // namespace ed
--- Segundo/ED/Practice_1/operadoresExternosMonomios.hpp
+++ Segundo/ED/Practice_1/operadoresExternosMonomios.hpp
@@ -15,6 +15,13 @@
 // Para la sobrecarga de los operadores de flujo: << y >>
 using std::istream;
 using std::ostream;
+using std::getline;
+
+//Para el uso de strings
+#include <string>
+using std::string;
+//Para el uso de funciones de conversion de cadenas
+#include <cstdlib>
 
 // Espacio de nombre de la asigantura: ed
 namespace ed
@@ -28,7 +35,7 @@ namespace ed
 		\param monomio_2: Referencia constante al monomio situado a la derecha del operador
 		\return true si los monomios tienen iguales coeficiente y grado, false en el caso contrario.
 	*/ 
-	bool operator==(ed::Monomio const & monomio_1, ed::Monomio const & monomio_2);
+	bool operator==(Monomio const & monomio_1, Monomio const & monomio_2);
 
 	/*!
 		\brief Operador de igualdad con monomio a la izquierda y real a la derecha del operador
@@ -37,7 +44,7 @@ namespace ed
 		\param numero_real: numero de tipo double que se compara con el monomio
 		\return true si el monomio es de grado 0 y su coeficiente es igual que numero_real, false en caso contrario
 	*/
-	bool operator==(ed::Monomio const & monomio, double numero_real);
+	bool operator==(Monomio const & monomio, double numero_real);
 
 	/*!
 		\brief Operador de igualdad con real a la izquierda del operador y monomio a la derecha
@@ -46,7 +53,7 @@ namespace ed
 		\param monomio: Referencia constante al monomio que se compara con el numero_real
 		\return true si el monomio es de grado 0 y su coeficiente es igual que numero_real, false en caso contrario
 	*/
-	bool operator==(double numero_real, ed::Monomio const & monomio);
+	bool operator==(double numero_real, Monomio const & monomio);
 
 	//! \name Operadores de desigualdad 
 
@@ -57,7 +64,7 @@ namespace ed
 		\param monomio_2: Referencia constante al monomio situado a la derecha del operador
 		\return true si los monomios tienen distinto grado o coeficiente, false en caso contrario
 	*/
-	bool operator!=(ed::Monomio const & monomio_1, ed::Monomio const & monomio_2);
+	bool operator!=(Monomio const & monomio_1, Monomio const & monomio_2);
 
 	/*!
 		\brief Operador de desigualdad entre un monomio a la izquierda del operador y un real a la derecha
@@ -66,7 +73,7 @@ namespace ed
 		\param numero_real: numero de tipo double que se compara con el monomio
 		\return true si el monomio no es de grado 0 o su coeficiente no es igual que el real pasado, false en caso contrario
 	*/
-	bool operator!=(ed::Monomio const & monomio, double numero_real);
+	bool operator!=(Monomio const & monomio, double numero_real);
 
 	/*!
 		\brief Operador de desigualdad entre un real a la izquierda del operador y un monomio a la derecha
@@ -75,57 +82,108 @@ namespace ed
 		\param monomio: Referencia constante al monomio que se compara con el real
 		\return true si el monomio no es de grado 0 o su coeficiente no es igual que el real pasado, false en caso contrario
 	*/
-	bool operator!=(double numero_real, ed::Monomio const & monomio);
+	bool operator!=(double numero_real, Monomio const & monomio);
 
 	//! \name Operadores unarios prefijos
 
 	/*!
-		\brief
+		\brief Operador unario prefijo + que devulve una copia del monomio que invoca el operador
+		\param monomio: Referencia al objeto de tipo monomio que invoca el operador
 	*/
-	ed::Monomio & operator+(ed::Monomio const & monomio);
-
+	Monomio & operator+(Monomio const & monomio);
+	
 	/*!
-		\brief
+		\brief Operador unario prefijo - que devuelve una copia negativa del monomio que invoca el operador
+		\post El objeto devuelto tiene el mismo grado que el inicial pero su coeficiente es el opuesto
 	*/
-	ed::Monomio & operator-(ed::Monomio const & monomio);
+
+	Monomio & operator-(Monomio const & monomio);
 
 	//! \name Operador binario de la suma 
 
 	/*!
-		\brief
+		\brief Operador + entre dos monomios
+		\param monomio_1: Referencia constante al monomio a la izquierda del operador
+		\param monomio_2: Referencia constante al monomio a la derecha del operador
+		\pre Ambos monomios deben tener el mismo grado
+		\return Monomio resultante de la suma de los monomios, mismo grado y suma de coeficientes
 	*/
-	ed::Monomio & operator+ (ed::Monomio const &monomio_1, ed::Monomio const &monomio_2);
+	Monomio & operator+(Monomio const &monomio_1, Monomio const &monomio_2);
 
 	//! \name Operador binario de la resta 
 
 	/*!
-		\brief
+		\brief Operador - entre dos monomios 
+		\param monomio_1: Referencia constante al monomio a la izquierda del operador
+		\param monomio_2: Referencia constante al monomio a la derecha del operador, que se resta a monomio_1
+		\pre Ambos monomios deben tener el mismo grado
+		\return Monomio resultante de la resta de los monomios, mismo grado y resta de coeficientes, NULL si no se cumple la precondicion
 	*/
-	ed::Monomio & operator- (ed::Monomio const &monomio_1, ed::Monomio const &monomio_2);
+	Monomio & operator-(Monomio const &monomio_1, Monomio const &monomio_2);
 
 	//! \name Operadores binarios de la multiplicación 
 
 	/*!
-		\brief
+		\brief Operador *  entre dos monomios
+		\param monomio_1: Referencia constante al monomio a la izquierda del operador
+		\param monomio_2: Referencia constante al monomio a la derecha del operador
+		\return Monomio resultante de operar, suma de grados y producto de coeficientes
 	*/
+	Monomio operator*(Monomio const &monomio_1, Monomio const &monomio_2);
+
+	/*!
+		\brief Operador * entre un numero real a la derecha y un monomio a la izquierda
+		\param numero_real: Numero que multiplica al coeficiente del monomio
+		\param monomio: Referencia constante al monomio pasado como argumento
+		\return Monomio resultante de la operacion
+	*/
+	Monomio & operator*(double numero_real, Monomio const &monomio);
+
+	/*!
+		\brief Operador * entre un numero real a la izquierda y un monomio a la derecha
+		\param numero_real: Numero que multiplica al coeficiente del monomio
+		\param monomio: Referencia constante al monomio pasado como argumento
+		\return Monomio resultante de la operacion
+	*/
+	Monomio & operator*(Monomio const &monomio, double numero_real);
 
 	//! \name Operadores binarios de la división 
 
 	/*!
-		\brief
+		\brief Operacion / entre dos monomios
+		\param monomio_1: Referencia constante al monomio a la izquierda del operador
+		\param monomio_2: Referencia constante al monomio a la derecha del operador, que divide a monomio_1
+		\return Monomio resultante de operar, resta de grados y division de coeficientes
 	*/
+	Monomio & operator/(Monomio const &monomio_1, Monomio const &monomio_2);
+
+	/*!
+		\brief Operador / entre un numero real a la derecha y un monomio a la izquierda
+		\param numero_real: Numero que divide al coeficiente del monomio
+		\param monomio: Referencia constante al monomio pasado como argumento
+		\return Monomio resultante de la operacion
+	*/
+	Monomio & operator/(double numero_real, Monomio const &monomio);
+
+	/*!
+		\brief Operador / entre un numero real a la izquierda y un monomio a la derecha
+		\param numero_real: Numero que divide al coeficiente del monomio
+		\param monomio: Referencia constante al monomio pasado como argumento
+		\return Monomio resultante de la operacion
+	*/
+	Monomio & operator/(Monomio const &monomio, double numero_real);
 
 	//! \name Sobrecarga de los operadores de entrada y salida 
 
 	/*!
 		\brief
 	*/
-	istream &operator>>(istream &stream, ed::Monomio &monomio);
+	istream &operator>>(istream &stream, Monomio &monomio);
 
 	/*!
 		\brief
 	*/
-	ostream &operator<<(ostream &stream, ed::Monomio const &monomio);
+	ostream &operator<<(ostream &stream, Monomio const &monomio);
 
 }  // Fin de namespace ed.
 
