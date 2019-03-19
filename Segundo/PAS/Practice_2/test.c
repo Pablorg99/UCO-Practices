7dcae6cc1ab330142c10124df4b6ffe7a8f6e552
Pablorg99 <i72rogup@uco.es>
2019-03-19 22:47:12 +0100

    updated test

--- Segundo/PAS/Practice_2/test.c
+++ Segundo/PAS/Practice_2/test.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
 	FILE *etc_group;
-	int character;
-	char *group_name;
+	char line[50];
+	char group_name[20];
 	etc_group = fopen("/etc/group", "r");
-	if(etc_group == NULL) perror("Can not open file");
-	while(!feof(etc_group)) {
-		printf("Im in while\n");	
-		fgetc(etc_group, "%s", line);
-		printf("%s\n", line);
+	while((fgets(line, 50, etc_group)) != NULL){
+		for(int i = 0; line[i] != ':'; i++) {
+			group_name[i] = line[i];
+		}
+		printf("%s\n", group_name);
 	}
-	
+	fclose(etc_group);
 	return 0;
 }
