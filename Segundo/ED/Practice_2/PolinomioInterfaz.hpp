5548963d2124f24fa048e7aab9e4921741c0795f
Pablorg99 <i72rogup@uco.es>
2019-03-29 16:04:08 +0100

    Virtual functions uncommented and documented

--- Segundo/ED/Practice_2/PolinomioInterfaz.hpp
+++ Segundo/ED/Practice_2/PolinomioInterfaz.hpp
@@ -14,38 +14,42 @@ namespace ed {
 //!  Definición de la clase PolinomioInterfaz
 class PolinomioInterfaz
 {
-
-   //! \name Funciones o métodos públicos de la clase
-   public:
-
-	//! \name Observadores: funciones de consulta
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
-  //Descomentar
-	//virtual bool esNulo() const = 0;
-
-  // COMPLETAR LOS COMENTARIOS DE DOXYGEN
-  //Descomentar
-	//virtual int getGrado()const = 0;
-
-  // COMPLETAR LOS COMENTARIOS DE DOXYGEN
-  //Descomentar
-	//virtual int getNumeroMonomios()const = 0;
-
-
-  // COMPLETAR LOS COMENTARIOS DE DOXYGEN
-  //Descomentar
-	//virtual bool existeMonomio(int n)const = 0;
-
-
-  // COMPLETAR LOS COMENTARIOS DE DOXYGEN
-  //Descomentar
-	//virtual ed::Monomio & getMonomio(int n) = 0;
-
-
-
+   	//! \name Funciones o métodos públicos de la clase
+	public:
+		//! \name Observadores: funciones de consulta
+		
+		/**
+		 * @brief Función virtual del observador esNulo()
+		 * @return 0 si no se implementa la función en la clase derivada (Polinomio)
+		 */
+		virtual bool esNulo() const = 0;
+		
+		/**
+		 * @brief Función virtual del observador del grado del polinomio
+		 * @return 0 si no se implementa la función en la clase derivada (Polinomio)
+		 */
+		virtual int getGrado() const = 0;
+		
+		/**
+		 * @brief Función virtual del observador del numero de monomios del polinomio
+		 * @return 0 si no se implementa la función en la clase derivada (Polinomio)
+		 */
+		virtual int getNumeroMonomios() const = 0;
+		
+		/**
+		 * @brief Función virtual del método que comprueba la existencia de un monomio
+		 * @param n: Grado del monomio en cuestión
+		 * @return 0 si no se implementa la función en la clase derivada (Polinomio)
+		 */
+		virtual bool existeMonomio(int n) const = 0;
+		
+		/**
+		 * @brief Observador que devuelve el monomio del grado indicado
+		 * @param n: Grado del monomio en cuestión
+		 * @return 0 si no se implementa la función en la clase derivada (Polinomio)
+		 */
+		virtual ed::Monomio & getMonomio(int n) = 0;
 }; // Fin de la definición de la clase PolinomioInterfaz
-
 
 } // \brief Fin de namespace ed.
 
