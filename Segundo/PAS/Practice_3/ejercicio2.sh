1791bee5d7ea6027aca47a52e7d812f5c58cf17c
Pablorg99 <i72rogup@uco.es>
2019-04-10 22:06:15 +0200

    Finished ej2 (-n :D)

--- Segundo/PAS/Practice_3/ejercicio2.sh
+++ Segundo/PAS/Practice_3/ejercicio2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ $# -lt 1 ] || [ $# -gt 2 ]; then
 	echo "La ejecución correcta es: $ ejercicio2.sh <directorio> [numero_bytes]"
 fi
@@ -11,19 +10,17 @@ else
 	n_bytes=0
 fi
 
-echo $n_bytes
-
-
 echo "Nombre LongitudUsuario FechaModificacion FechaAcceso Tamaño Bloques Permisos Ejecutable"
-for file in $(find $1 -size +`$n_bytes`c -type f -or -size `$n_bytes`c -type f)
+for file in $(find $1 -size +"$n_bytes"c -type f -or -size "$n_bytes"c -type f)
 do
 	LongitudUsuario=$(stat -c %U $file | wc -m)
-	Permisos=$(stat -c %A $file)
-	if [ Permisos[4] == "x" ]; then
+
+	if [[ -x "$file" ]]
+	then
 		Ejecutable=1
 	else
 		Ejecutable=0
 	fi
 
-	echo "$(basename $file);$LongitudUsuario;$(stat -c %y $file);$(stat -c %x $file);$(stat -c %s $file);$(stat -c %b $file);$Permisos;$Ejecutable)"
-done
+	echo "$(basename $file);$LongitudUsuario;$(stat -c '%y;%X;%s;%b;%A' $file);$Ejecutable)"
+done | sort -k 5 -n -t ";"
