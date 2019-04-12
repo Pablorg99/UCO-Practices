f0892a0cbd64d40356df29750eff43e9ace1fcb9
Pablorg99 <i72rogup@uco.es>
2019-04-12 21:47:42 +0200

    Exercise 6 WIP

--- Segundo/PAS/Practice_3/ejercicio6.sh
+++ Segundo/PAS/Practice_3/ejercicio6.sh
@@ -1,8 +1,14 @@
-# arg1: carpeta
+#!/bin/bash
+
+# Check that the arguments are correct
+if [ $# -ne 1] || [ -d "$1" ] ; then
+	echo "La ejecución correcta es: $ ejericico6.sh <nombre_directorio>"
+fi
+
 
 echo "<html>"
 echo "<head>"
-echo "<title><h3> Listado directiorios de $1</h3></title>"
+echo "<title><h3> Listado directiorios de $1 </h3></title>"
 echo "</head>"
 echo "<body>"
 # listar archivos $1
@@ -10,9 +16,10 @@ echo "</body>"
 echo "</html>"
 
 # FUNCTION
-function listar_archivos()
+function listar_archivos() {
 	echo "<ul>"
 	for x in $(find $1 -maxdepth 1) </ul>
 	do
 		<strong><li>$x</strong></li>
 	done
+}
