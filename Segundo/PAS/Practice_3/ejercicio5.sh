0883b37d5911d18e9043e38cdab2cbc230287fc8
Pablorg99 <i72rogup@uco.es>
2019-04-12 20:07:20 +0200

    Exercise 5 finished

--- Segundo/PAS/Practice_3/ejercicio5.sh
+++ Segundo/PAS/Practice_3/ejercicio5.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-# ficheros $* 
-date=$(date +%s)
-for $file in $(find copiaseg -type f)
+
+# If "~/Copia" doesn't exist, create it
+if [ ! -d "$HOME/Copia" ]; then
+	echo "Creando el directorio ~/Copia"
+	mkdir ~/Copia
+fi
+
+# Iterates for each file in ~/Copia
+# If $file is more than 200 seconds old, remove it
+for file in $(find ~/Copia -type f)
 do
-	creation=$(stat $file -c %x)
-	time=$date-$creation
+	creation=$(stat $file -c %X)
+	let time=$(date +%s)-$creation
+	if [ $time -gt 5 ]; then
+		echo "Borrando $file de $time segundos..."
+		rm -f $file
+	fi
 done
-tar -cfx copiaseg/copia-$(whioami)-$date.tar.g $* 
+
+# If there are 1 or more file/folder passed as argument, create a backup with them
+if [ $# -gt 0 ]; then
+	tar -czf ~/Copia/copia-$(whoami)-$(date +%s).tar.gz $*
+	echo "La copia de seguridad se ha creado correctamente."
+else 
+	echo "Dado que no se ha especificado ningún fichero/carpeta como argumento, no se ha creado una copia de seguridad"
+fi
+
+
