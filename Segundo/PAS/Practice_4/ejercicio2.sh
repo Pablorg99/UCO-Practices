b797f8cd764e5760236db5d4754dd0423107ff76
Pablorg99 <i72rogup@uco.es>
2019-05-22 16:14:59 +0200

    ejercicio2 WIP

--- Segundo/PAS/Practice_4/ejercicio2.sh
+++ Segundo/PAS/Practice_4/ejercicio2.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+if [[ $# -ne 1 || ! -f $1 ]]; then
+	echo "La ejecución correcta del programa es: ejercicio2.sh <rutaFichero.txt>"
+	exit 1
+fi
+
+grep -E '^.+[^=+]$' peliculas.txt | sed -r 
+'
+	
+'  
