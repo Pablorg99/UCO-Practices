d54852e74533828e0bcda0c8901ca7841f3e8d6b
Pablorg99 <i72rogup@uco.es>
2019-05-25 16:40:52 +0200

    Exercise 3 WIP

--- Segundo/PAS/Practice_4/ejercicio3.sh
+++ Segundo/PAS/Practice_4/ejercicio3.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+if [ $# -gt 1 ]; then
+    echo "La ejecución correcta del programa tiene la siguiente forma: ejercicio2.sh [rutaFichero]"
+fi
+
+echo "================"
+echo "Listado de los archivos ocultos del directorio $HOME"
+ls -a $HOME | grep -E '^\..*'
+
+echo "================"
+if [[ $# -ne 1 || ! -f $1 ]]; then
+    echo "Si quiere hacer una copia de un fichero sin líneas vacías, añada la ruta del directorio como argumento del script"
+else 
+    echo "El fichero $1 sin lineas vacías se ha guardado en $1.sinLineasVacias"
+    sed -nr 's/(^.+$)/\1/p' $1 > $1.sinLineasVacias
+fi
+
+echo "================"
