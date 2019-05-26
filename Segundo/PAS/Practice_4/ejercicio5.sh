0081a4c6c1a62b1f05a85a7dada85f656990cdea
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-05-26 21:13:29 +0200

    Exercise 5 and 6 finished, P4 done!

--- Segundo/PAS/Practice_4/ejercicio5.sh
+++ Segundo/PAS/Practice_4/ejercicio5.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+if [[ $# -ne 0 ]]; then
+    echo "Este script no requiere argumentos, si añade alguno no se utilizará."
+fi
+
+# El primer sed hace que solo se muestren las lineas necesarias, si no se incluye se mostrarían las características de cada procesador.
+# El segundo sed hace que se seleccionen las referencias de los datos pedidos por el enunciado con una expresión que empareje con el formato del fichero.
+
+cat /proc/cpuinfo | sed -n '2,13p' | sed -rn 's/model name[\t]*: (.+)/Modelo de procesador: \1/p; s/cpu MHz[\t]*: (.+)/Megahercios: \1/p; s/cpu cores[\t]*: (.+)/Número máximo de hilos en ejecución: \1/p; s/cache size[\t]*: (.+)/Tamaño de caché: \1/p; s/vendor_id[\t]*: (.+)/ID vendedor: \1/p'
+
+# Al igual que en el ejercicio 3, cada [^ ]+ son los caracteres de una columna, se ponen entre parentesis los datos exigidos y se establece el formato en la segunda parte del comando s
+cat /proc/mounts | sed -rn 's/^([^ ]+)\s+([^ ]+)\s+([^ ]+).*/Punto de montaje: \2, Dispositivo: \1, Tipo de dispositivo: \3/p'
+
+# Misma estructura que el apartado anterior
+cat /proc/partitions | sed -rn 's/^\s+[^ ]+\s+[^ ]+\s+([^ ]+)\s+([^ ]+).*/Partición: \2, Número Bloques: \1/p'
--- Segundo/PAS/Practice_4/ejercicio6.sh
+++ Segundo/PAS/Practice_4/ejercicio6.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "La ejecución correcta del programa tiene la siguiente forma: ejercicio6.sh <rutaShellPorDefecto>"
+    exit 1
+fi
+# La expresión regular empareja con todas las líneas ya que todas utilizan el mismo formato. El carácter separador de campos es ':' y se ponen entre paréntesis los campos que va incluir la nueva cadena.
+grep -E "$1" /etc/passwd | sed -re "s/^(.+):x:([0-9]+):([0-9]+):(.*):(.+):(.+)/=================\nLogname: \1\n->UID: \2\n->GID: \3\n->gecos: \4\n->Home: \5\n->Shell por defecto: \6/"
