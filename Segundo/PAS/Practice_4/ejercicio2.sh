ce992035fce4089379f32610d904d77748b67ae5
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-05-26 17:17:51 +0200

    Exercise 2 finished

--- Segundo/PAS/Practice_4/ejercicio2.sh
+++ Segundo/PAS/Practice_4/ejercicio2.sh
@@ -5,17 +5,14 @@ if [[ $# -ne 1 || ! -f $1 ]]; then
 	exit 1
 fi
 
-# |-> Fecha de estreno: 10/03/2017
-# |-> Director: Jordan Vogt-Roberts
-# |-> Reparto: Tom Hiddleston, Samuel L. Jackson, Brie Larson...
-# |-> Fecha de estreno: 03/03/2017
-# |-> Director: James Mangold
-# |-> Reparto: Hugh Jackman, Patrick Stewart, Dafne Keen...
+# ^[=]*$ ==> Líneas vacías o que solo contengan el carácter '=' un número cualquiera de veces
 
-grep -E '^.+[^=+]$' peliculas.txt | sed -r 
-'
-	s/\(([0-9]{2}\/[0-9]{2}\/[0-9]{4})\)/|-> Fecha de estreno: \1/g;
-    s/Dirigida por (.+)/|-> Director: \1/g;
-    s/Reparto: (.+)/|-> Reparto: \1/g;
-    s/^([1-9]hr\s.*min)/|-> Duración: \1/g;
-'  
+grep -v -E "^[=]*$" $1 | sed -r '/^ .+$/d; s/\(([0-9]{2}\/[0-9]{2}\/[0-9]{4})\) \-.+\-/|-> Fecha de estreno: \1/g; s/^Dirigida por (.+)$/|-> Director: \1/g; s/^Reparto: (.+)$/|-> Reparto: \1/g; s/([0-9]hr [0-9]{2}min)/|-> Duración: \1/g; s/^([^|].+)/Titulo: \1/g'
+
+# Emparejamientos de expresiones regulares del sed:
+# 1: ^ .+$/ ==> Empareja con las líneas que empiezan por un espacio (la sinopsis de las películas)
+# 2: (([0-9]{2}\/[0-9]{2}\/[0-9]{4})\) \-.+\- ==> Empareja con líneas con el formato: (nn/nn/nnnn) -c- donde n es un dígito y c son un número cualquiera de caracteres cualesquiera. Los primeros paréntesis se escapan ya que quieren ser interpretados literalmente, no como argumento de 's'
+# 3: ^Dirigida por (.+)$ ==> El argumento \1 corresponde a los carácteres que hay desde el final de 'Dirigida por ' hasta el final de la línea
+# 4: ^Reparto: (.+)$ ==> Misma estructura de expresión regular que el apartado anterior
+# 5: ([0-9]hr [0-9]{2}min) ==> Líneas del tipo: nhr nnmin donde n es un dígito cualquiera
+# 6: ^([^|].+) ==> Todas las líneas que no empiezan por '|', que llegado a este punto, solo son títulos de película
