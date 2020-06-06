ca6c7bbbfc3959b4085b37c15d81d9c49ed93a37
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-06-07 01:59:32 +0200

    Fixed bug in inrange rule

--- Tercero/ISSBC/TFP/rules/in_range_rule.py
+++ Tercero/ISSBC/TFP/rules/in_range_rule.py
@@ -1,6 +1,7 @@
 class InRangeRule:
     @staticmethod
     def satisfied(value_to_check, correct_values):
-        if correct_values[0] <= value_to_check <= correct_values[1]:
+
+        if (int(value_to_check) >= int(correct_values[0])) and (int(value_to_check) <= int(correct_values[1])):
             return True
         return False
